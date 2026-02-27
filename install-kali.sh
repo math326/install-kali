559cf0618ff155f03462bd8518da064dece2e682
math326 <ofertasonline520@gmail.com>
2026-02-27 20:03:27 -0300

    mudanças 27/02/2026

--- install-kali.sh
+++ install-kali.sh
@@ -20,6 +20,6 @@ echo "3) Atualizando pacotes..."
 sudo apt update
 
 echo "4) Instalando metapacote kali-linux-everything..."
-sudo apt install nmap metasploit-framework hydra aircrack-ng john sqlmap wireshark tcpdump ncat netcat whatweb gobuster -y
+sudo apt install nmap metasploit-framework hydra aircrack-ng john sqlmap wireshark tcpdump ncat whatweb gobuster -y
 
 echo "Instalação concluída!"
